51c43329315c116e5247ce5ec47f4cb4187ffb49
Vadim Solovei <vadims.olovei90@gmail.com>
2021-05-28 21:17:26 +0300

    deleted unused tasks and packages

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,1 @@
-import $ from '../local_modules/jquery/dist/jquery.min'
-
-$(document).ready(() => {
-    // eslint-disable-next-line no-console
-    console.log(`document ready`)
-})
+console.log(`document ready!`);
